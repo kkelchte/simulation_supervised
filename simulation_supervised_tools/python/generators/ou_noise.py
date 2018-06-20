e980e902a9ae7729069375a78d1f920d96aad0fb
klaas kelchtermans <klaas.kelchtermans@esat.kuleuven.be>
2018-06-20 12:40:47 +0200

    update

---
+++ simulation_supervised_tools/python/generators/ou_noise.py
@@ -0,0 +1,46 @@
+# --------------------------------------
+# Reference: https://github.com/rllab/rllab/blob/master/rllab/exploration_strategies/ou_strategy.py
+# --------------------------------------
+
+import numpy as np
+import numpy.random as nr
+
+
+class OUNoise:
+
+    """ docstring for OUNoise """
+    def __init__(self,action_dimension,mu=0, theta=0.15, sigma=1.0):
+        # nr.seed(201)
+        self.action_dimension = action_dimension
+        self.mu = mu
+        self.theta = theta
+        self.sigma = sigma
+        self.state = np.ones(self.action_dimension) * self.mu
+        self.reset()
+
+    def reset(self):
+        self.state = np.ones(self.action_dimension) * self.mu
+
+    def noise(self):
+        x = self.state
+        dx = self.theta * (self.mu - x) + self.sigma * nr.randn(len(x))
+        self.state = x + dx
+        return self.state
+
+if __name__ == '__main__':
+    # for theta, sigma in [ (0.0015,0.00003), (0.003,0.00015), (0.006,0.0006), (0.015,0.003), (0.03,0.015), (0.06,0.06), (0.15,0.3) ]: 
+    # for theta in [ 0.0015, 0.003, 0.006, 0.015, 0.03, 0.06, 0.15 ]:
+    theta=0.15
+    sigma=1
+    amp=0.01
+    print('theta: ', theta)
+    ou = OUNoise(4,0,theta,sigma)
+    states = []
+    total=100
+    for i in range(total):
+      #if i%100: print(i," of ",total)
+      #states.append(nr.rand()-0.5)
+      states.append(amp*ou.noise())
+    import matplotlib.pyplot as plt
+    plt.plot(states)
+    plt.show()
