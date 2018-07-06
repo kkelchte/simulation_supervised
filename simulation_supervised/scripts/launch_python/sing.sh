2d17840d4c6e1ad545680e788da1acd11ab03685
klaas kelchtermans <klaas.kelchtermans@esat.kuleuven.be>
2018-07-06 15:47:48 +0200

    update:

--- simulation_supervised/scripts/launch_python/sing.sh
+++ simulation_supervised/scripts/launch_python/sing.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-export PYTHONPATH=$HOME/tensorflow/pilot:\
-$HOME/drone_ws/devel/lib/python2.7/dist-packages:\
-/opt/ros/kinetic/lib/python2.7/dist-packages:\
-$HOME/simsup_ws/devel/lib/python2.7/dist-packages:\
-/usr/lib/python2.7/dist-packages:\
-/usr/local/lib/python2.7/dist-packages
+# export PYTHONPATH=$HOME/tensorflow/pilot:\
+# $HOME/drone_ws/devel/lib/python2.7/dist-packages:\
+# /opt/ros/kinetic/lib/python2.7/dist-packages:\
+# $HOME/simsup_ws/devel/lib/python2.7/dist-packages:\
+# /usr/lib/python2.7/dist-packages:\
+# /usr/local/lib/python2.7/dist-packages
 
-export LD_LIBRARY_PATH=$HOME/simsup_ws/devel/lib:\
-$HOME/drone_ws/devel/lib:/opt/ros/kinetic/lib:\
-/usr/local/cuda/lib64:/usr/local/cudnn/lib64:\
-/.singularity.d/libs
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64:/usr/local/cudnn/lib64:
 
 python $@
 
